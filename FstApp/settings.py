0f75110201a5c17d5e697fde3843cb10b3d9fb15
ElnSantiago <ellian.santiago@medway.com.br>
2022-07-17 20:03:41 -0300

    Collect static

--- FstApp/settings.py
+++ FstApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!l7zd9m9yi=$lh4x=kbcd3q27(4=#e2r4=hbhol(m0dacqp$=_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+DASHBOARD_URL = 'https://192.168.100.4:8000/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
